ee57374a0e401a4b703b8b43bcaf968d54229360
xinlinux <xinli13621862166@hotmail.com>
2021-08-16 15:32:15 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,30 @@
-game.startCountdown(10000)
-music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+basic.showNumber(3105897)
+game.startCountdown(600000)
+basic.forever(function () {
+    basic.showNumber(3105897)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showNumber(3105897 - 82348)
+    basic.showLeds(`
+        # # . . #
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showNumber(3105897 - (82348 + 82340))
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # . . #
+        # . . . #
+        . . . . .
+        `)
+    basic.showNumber(3105889 - 4e+40)
+    basic.showString("Game over")
+})
