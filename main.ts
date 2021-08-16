eeefc16fe8cffbdcbf58eeb4f313f32dfa9373b3
xinlinux <xinli13621862166@hotmail.com>
2021-08-16 10:24:45 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,121 @@
-let i = 1
 basic.forever(function () {
-    basic.showNumber(i)
+    basic.showNumber(0)
+    basic.showLeds(`
+        # # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     basic.pause(1000)
-    i += 1
+    basic.showLeds(`
+        . # # # #
+        # # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . # . #
+        . # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        # . # . #
+        . # # # #
+        # # # # .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . # . #
+        # # # # .
+        # . # . #
+        . # # # #
+        . # # . .
+        `)
+    basic.pause(1000)
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # . # #
+        . . # . #
+        # . . # .
+        . . # . #
+        . . # # #
+        `)
+    basic.pause(1000)
+    basic.showNumber(2 + 3)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # # . # #
+        . . # . #
+        # . . # .
+        . . . . #
+        `)
+    basic.pause(1000)
+    basic.showNumber(2 + (3 + 1))
+    basic.pause(1000)
+    basic.showLeds(`
+        # # . . .
+        . . . # #
+        # . . # #
+        . . . . #
+        # . . . .
+        `)
+    basic.pause(1000)
+    basic.showNumber(2 + (3 + (1 + 1)))
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # . # #
+        # # . . #
+        `)
+    basic.pause(1000)
+    basic.showNumber(2 + (3 + (1 + (1 + 3))))
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # #
+        # . # . #
+        . # # . #
+        # . # . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showNumber(2 + (3 + (1 + (1 + (3 + 5 * (5 * 4))))))
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showNumber(5 * (5 * (4 + (5 + 4))))
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showString("Game over")
 })
