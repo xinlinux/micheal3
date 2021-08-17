41b3bccb1b1b091303af65d9a818bf7b1282dfc8
xinlinux <xinli13621862166@hotmail.com>
2021-08-17 09:15:41 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,7 @@
-basic.pause(60000)
-music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+game.startCountdown(10000)
+for (let index = 0; index < 2; index++) {
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Forever)
+}
+basic.forever(function () {
+    basic.showNumber(40)
+})
