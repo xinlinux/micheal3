0ee53c93f6ff841874caf77dd34062f152fbb3a2
xinlinux <xinli13621862166@hotmail.com>
2021-08-17 15:29:49 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    game.pause()
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.West)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 1; index++) {
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+    }
+    basic.showArrow(ArrowNames.North)
+})
+input.onButtonPressed(Button.B, function () {
+    game.resume()
+    basic.showArrow(ArrowNames.South)
+})
+led.setBrightness(255)
 game.startCountdown(10000)
 for (let index = 0; index < 2; index++) {
-    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Forever)
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
 }
+basic.showArrow(ArrowNames.East)
 basic.forever(function () {
+    radio.setGroup(255)
     basic.showNumber(40)
 })
